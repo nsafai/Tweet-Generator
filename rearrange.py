1a102f73aebd8148a083d535323c675d2063363e
Nicolai Safai <nicolaisafai@gmail.com>
2018-10-23 17:05:55 -0700

    reversed string

--- rearrange.py
+++ rearrange.py
@@ -1,7 +1,11 @@
 import sys # to access command line arguments
 import random # to generate random numbers
 
+def reversed_string(a_string):
+    return a_string[::-1]
+
 if __name__ == '__main__':
     params= sys.argv[1:] # take a sublist starting from index 1 till the end
     random.shuffle(params)
+    params[1] = reversed_string(params[1])
     print(params)
