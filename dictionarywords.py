a2f6055332df593d8cb74341ee09a5d7631185b4
Nicolai Safai <nicolaisafai@gmail.com>
2018-10-30 15:46:33 -0700

    cleanup

--- dictionarywords.py
+++ dictionarywords.py
@@ -2,24 +2,27 @@ import sys # to access command line arguments
 import random # to generate random numbers
 
 def readFile(url):
-    f = open(url, 'r')
-    content = f.read().splitlines()
+    f = open(url, 'r') # open file at URL
+    content = f.read().splitlines() # split file up lines and add each linne to an array named 'content'
     f.close()
-    # print(content)
+
     return content
+
+def returnRandomWords(content, num_words):
+    sentence = []
+    count = 0
+    while count < num_words:
+        random_word = random.choice(content)
+        sentence.append(random_word)
+        count+= 1
+
+    return sentence
+
 
 if __name__ == '__main__':
     params = sys.argv[1:] # take a list of arguments, starting from index 1 till the end
     num_words = int(params[0]) # num_words stores the number of words to use when generating a sentence
+    content = readFile('/usr/share/dict/words') # read /usr/share/dict/words
 
-    # read /usr/share/dict/words
-    content = readFile('/usr/share/dict/words')
-    sentence = []
-    count = 0
-    while count < num_words:
-        sentence.append(random.choice(content))
-        count+= 1
-
-    for word in sentence:
-        print(word)
-    # print(*sentence) # using a "*" removes the weird syntax you usually see when printing a list
+    random_words = returnRandomWords(content, num_words)
+    print(*random_words) # using a * removes the ugly "[" syntax from the console output
