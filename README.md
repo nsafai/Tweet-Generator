cf29e01dc154c04e2bceafaadea5897774046829
Nicolai Safai <nicolaisafai@gmail.com>
2018-10-29 19:20:02 -0700

    choosing random words from local txt file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Tweet Generator
+
+## Summary
 Building a tweet generator with the modest goal of passing the Turing test using the [following tutorial](https://www.makeschool.com/academy/track/tweet-generator--data-structures---probability-with-python).
+
+## Milestones
+** Step 1. Rearranging words from terminal output.** To test this, clone the repo and run `python3 rearrange.py some random words`
---
+++ dictionarywords.py
@@ -0,0 +1,23 @@
+import sys # to access command line arguments
+import random # to generate random numbers
+
+def readFile(url):
+    f = open(url, 'r')
+    content = f.read().splitlines()
+    f.close()
+    # print(content)
+    return content
+
+if __name__ == '__main__':
+    params = sys.argv[1:] # take a list of arguments, starting from index 1 till the end
+    num_words = int(params[0]) # num_words stores the number of words to use when generating a sentence
+
+    # read /usr/share/dict/words
+    content = readFile('words.txt')
+    sentence = []
+    count = 0
+    while count < num_words:
+        sentence.append(random.choice(content))
+        count+= 1
+
+    print(*sentence) # using a "*" removes the weird syntax you usually see when printing a list
