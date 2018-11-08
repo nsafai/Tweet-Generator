ce803a0015275f4051ec7de09eb2624416536d40
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-08 11:31:54 -0800

    cleanup draft complete

--- app.py
+++ app.py
@@ -4,57 +4,31 @@ import tokenize # turn source file into a list of tokens
 import word_count # to get a histogram from source file
 import sample # to generate a random sample of __ words from a source file
 # import sentence
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-# define some functions that compose the above modules
-def readFile(source_text):
-    return cleanup.readFile(source_text)
+# --- FUNCTIONS BASED ON MODULES IMPORTED ABOVE ---
+def sampleWords(source_text, num_words):
+    clean_content = cleanup.readFile(source_text)
+    list_of_tokens = tokenize.listOfTokens(content)
+    histogram = word_count.histogramDict(tokens)
+    dict_of_probability = sample.dictionaryOfProbability(histogram)
+    sample_words = sample.generateRandomSentence(dictionaryOfProbability, num_words)
+    return sample_words
 
-def listOfTokens(content):
-    return tokenize.listOfTokens(content)
+# --- ROUTES ---
+@app.route('/')
+def index():
+    num_words = 23
+    sample_words = sampleWords('txt-files/edgarallanpoe.txt', num_words)
+    return sample_words
 
-def histogram(tokens):
-    return word_count.histogramDict(tokens)
-
-def dictionaryOfProbability(histogram):
-    return sample.dictionaryOfProbability(histogram)
-
-def generateRandomSentence(dictionaryOfProbability, num_words):
-    return sample.generateRandomSentence(dictionaryOfProbability, num_words)
-
-# @app.route('/')
-# def index():
-#     num_words = 20
-#     content = stochasticsampling.readFile('txt-files/edgarallanpoe.txt')
-#     histogram = stochasticsampling.histogramDict(content)
-#     dictionaryOfProbability = stochasticsampling.dictionaryOfProbability(histogram)
-#     sentence, test_dict = stochasticsampling.generateRandomSentence(dictionaryOfProbability, num_words)
-#
-#     return str(sentence)
-#
-# @app.route('/<num_words>')
-# def generate_sentence(num_words):
-#     # show the user profile for that user
-#     content = stochasticsampling.readFile('txt-files/edgarallanpoe.txt')
-#     histogram = stochasticsampling.histogramDict(content)
-#     dictionaryOfProbability = stochasticsampling.dictionaryOfProbability(histogram)
-#     sentence, test_dict = stochasticsampling.generateRandomSentence(dictionaryOfProbability, int(num_words))
-#
-#     return str(sentence)
+@app.route('/<num_words>')
+def generate_sentence(num_words):
+    sample_words = sampleWords('txt-files/edgarallanpoe.txt', num_words)
+    return sample_words
 
 if __name__ == '__main__':
     # code to run when file is executed
-    # app.debug = True
-    # app.run(host='0.0.0.0' , port=5000)
-
-    params = sys.argv[1:] # take a list of arguments, starting from index 1 till the end
-    source_text = str(params[0]) # url for source_text
-    num_words = int(params[1]) # number of sample words to generate
-
-    content = readFile(source_text)
-    list_of_tokens = listOfTokens(content)
-    histogram = histogram(list_of_tokens)
-    dictionaryOfProbability = dictionaryOfProbability(histogram)
-    sentence, test_dict = generateRandomSentence(dictionaryOfProbability, num_words)
-    print(sentence)
+    app.debug = True
+    app.run(host='0.0.0.0' , port=5000)
