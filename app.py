45af5674fdbdb1cd6d607bbd1212d38e631fd129
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-07 21:34:17 -0800

    Debuggn

--- app.py
+++ app.py
@@ -35,6 +35,11 @@ def generate_sentence(number):
 
     return str(sentence)
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0' , port=5000)
