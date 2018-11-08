e4b886b0786220d0c4c455fd7d573fd8a1601b4f
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-07 21:37:29 -0800

    url change

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
 
     return str(sentence)
 
-@app.route('/gen/<number>')
+@app.route('/<number>')
 def generate_sentence(num_words):
     # show the user profile for that user
     content = stochasticsampling.readFile('txt-files/edgarallanpoe.txt')
