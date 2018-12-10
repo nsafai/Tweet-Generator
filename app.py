00f9149640c2fdc99deae3ec10fb133981a89219
Nicolai Safai <nicolaisafai@gmail.com>
2018-12-09 22:47:45 -0800

    got beta version of beatles lyrics working

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 import sys # for command line args
 import cleanup # to cleanup source file
 import tokenizer # turn source file into a list of tokens
@@ -7,22 +8,48 @@ from markov_dictogram import MarkovDictogram
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-# OPEN UP SOURCE FILE ONCE ONLY
-markov_order = 3
-source_text = 'txt-files/alice.txt'
-content = cleanup.readFile(source_text)
-list_of_tokens = tokenizer.listOfTokens(content)
-# word_histogram = word_count.histogramDict(list_of_tokens)
-markov_dictogram = MarkovDictogram(list_of_tokens, markov_order)
+# ---dotenv setup---
+import os
+from dotenv import load_dotenv
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
 
-# --- ROUTES ---
+# ---genius API setup---
+import lyricsgenius as genius
+genius_client_token = os.getenv("GENIUS_CLIENT_ACCESS_TOKEN")
+api = genius.Genius(genius_client_token)
+
+
+# ---IMPORT ALICE IN WONDERLAND---
+alice_markov_order = 2
+alice_corpus_url = 'txt-files/alice.txt'
+alice_content = cleanup.readFile(alice_corpus_url)
+alice_tokens = tokenizer.listOfTokens(alice_content)
+alice_dictogram = MarkovDictogram(alice_tokens, alice_markov_order)
+
+# -- IMPORT BEATLES LYRICS --
+beatles_markov_order = 3
+beatles_corpus_url = 'beatles_lyrics.json'
+beatles_content = cleanup.readAndCleanGeniusLyrics(beatles_corpus_url)
+beatles_tokens = tokenizer.listOfTokens(beatles_content)
+beatles_dictogram =  MarkovDictogram(beatles_tokens, beatles_markov_order)
+
+
+
+# ---ROUTES---
 @app.route('/')
 def index():
     num_words = request.args.get('num', default=40, type=int)
-    # markov_sentence = sample.generateMarkovSentence(word_histogram, markov_dictogram, num_words)
-    markov_sentence = sample.generateNthOrderMarkovSentence(markov_dictogram, num_words, markov_order)
+    theme = request.args.get('theme', default='alice')
+    if theme == 'beatles':
+        markov_sentence = sample.generateNthOrderMarkovSentence(beatles_markov_order, num_words, beatles_markov_order)
+    else:
+        markov_sentence = sample.generateNthOrderMarkovSentence(alice_dictogram, num_words, alice_markov_order)
+
     return render_template('home.html', markov_sentence=markov_sentence)
 
+
+# ---RUN CODE---
 if __name__ == '__main__':
     # code to run when file is executed 'flask run'
     app.debug = True
--- cleanup.py
+++ cleanup.py
@@ -7,12 +7,33 @@ def readFile(url):
     file = open(url, 'r')
     content = file.read()
     content_wo_contractions = decontracted(content)
-    # content_wo_symbols = re.sub(r'[^\w]', ' ', content_wo_contractions);
+    content_wo_symbols = re.sub(r'[^\w]', ' ', content_wo_contractions);
     content_wo_mult_spaces = re.sub(r"\s+"," ", content_wo_contractions, flags = re.I)
     # clean_content_lower = content_wo_mult_spaces.lower()
     clean_content = re.sub(r" i ", " I ", content_wo_mult_spaces)
     file.close()
     return clean_content
+
+def readAndCleanGeniusLyrics(url):
+    file = open(url, 'r')
+    raw_lyrics_content = file.read()
+
+    # -- GENIUS LYRICS CLEANUP --
+    lyrics_stage_1 = re.findall('\"lyrics\": \".*?\"', raw_lyrics_content) # grab content after "lyrics" key in JSON file
+    lyrics_stage_2 = re.sub(r'\"lyrics\"', ' ', str(lyrics_stage_1))
+    lyrics_stage_3 = re.sub(r'\\n', '\n', str(lyrics_stage_2)) # remove "\n"
+    lyrics_stage_4 = re.sub(r'\\', ' ', str(lyrics_stage_3)) # remove "\"
+    lyrics_stage_5 = re.sub(r', , ', ' ', str(lyrics_stage_4)) # remove misc commas
+    lyrics_stage_6 = re.findall('(?<=\]).+?(?=\[.*?)', str(lyrics_stage_5)) # get text between [Verse _]
+    lyrics_stage_7 = decontracted(str(lyrics_stage_6)) # remove contractions before removing '
+    lyrics_stage_8 = re.sub(r'[^\w\s?\.]', '', str(lyrics_stage_7)) # remove symbols except ? and .
+    lyrics_stage_9 = re.sub(r'\s+'," ", str(lyrics_stage_8), flags = re.I) # wo multiple spaces
+    lyrics_stage_10 = re.sub(r'u[0-9].*?', " ", str(lyrics_stage_9)) # remove weird u82793817 tokens
+    lyrics_stage_11 = re.sub(r'\d', "", str(lyrics_stage_10))
+    # print(str(lyrics_stage_8))
+    file.close()
+
+    return str(lyrics_stage_11)
 
 def decontracted(phrase):
     # taken from https://stackoverflow.com/a/47091490/3247102
